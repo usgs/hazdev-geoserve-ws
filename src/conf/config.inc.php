a002a31ddd7a6546f1bd8e7ef3153b0b421766a3
Jeremy Fee <jmfee@usgs.gov>
2015-02-20 18:53:44 -0700

    Configure geoserve factory

--- src/conf/config.inc.php
+++ src/conf/config.inc.php
@@ -2,10 +2,16 @@
 
 date_default_timezone_set('UTC');
 
-$CONFIG_INI_FILE = dirname(__FILE__) . '/config.ini';
+$APP_DIR = dirname(dirname(__FILE__));
+
+// parse config
+$CONFIG_INI_FILE = $APP_DIR . '/conf/config.ini';
 if (!file_exists($CONFIG_INI_FILE)) {
   trigger_error('Application not configured. Run pre-install script.');
   exit(-1);
 }
-
 $CONFIG = parse_ini_file($CONFIG_INI_FILE);
+
+// configure factory
+include $APP_DIR . '/lib/classes/GeoserveFactory.class.php';
+$FACTORY = new GeoserveFactory($CONFIG['DB_DSN'], $CONFIG['DB_USER'], $CONFIG['DB_PASS']);
