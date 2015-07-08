7d85933de48b96699366e1c39fef6c88a3aabfb7
Eric Martinez <emartinez@usgs.gov>
2015-07-08 14:16:00 -0600

    Added rectangle parameters to places query.

--- src/lib/classes/PlacesQuery.class.php
+++ src/lib/classes/PlacesQuery.class.php
@@ -5,6 +5,12 @@ class PlacesQuery {
   public $latitude = null;
   public $longitude = null;
 
+  public $minlatitude = null;
+  public $maxlatitude = null;
+
+  public $minlongitude = null;
+  public $maxlongitude = null;
+
   // places
   public $maxradiuskm = null;
   public $minpopulation = null;
