5031a8b84fef7223249aab33c1d4d27cbd12294c
Eric Martinez <emartinez@usgs.gov>
2015-07-08 17:02:26 -0600

    Implemented search by rectangle. Refactored search by circle so common code is not repeated.

--- src/lib/classes/PlacesFactory.class.php
+++ src/lib/classes/PlacesFactory.class.php
@@ -22,21 +22,36 @@ class PlacesFactory extends GeoserveFactory {
    *         if at least one of $query->limit or $query->maxradiuskm
    *         is not specified.
    */
-  public function get($query, $callback=null) {
+  public function get ($query, $callback=null) {
+
+    if ($query->latitude !== null && $query->longitude !== null &&
+        $query->maxradiuskm !== null) {
+      return $this->getByCircle($query, $callback);
+    } else if ($query->minlatitude !== null && $query->maxlatitude !== null &&
+        $query->minlongitude !== null && $query->maxlongitude !== null) {
+      return $this->getByRectangle($query, $callback);
+    } else {
+      throw new Exception('Neither circle nor rectangle parameters provided.');
+    }
+  }
+
+  /**
+   * Get nearby places for circle searches.
+   *
+   */
+  public function getByCircle ($query, $callback = null) {
     if ($query->latitude === null || $query->longitude === null ||
         $query->maxradiuskm === null) {
       throw new Exception('"latitude", "longitude", and "maxradiuskm"' .
           ' are required');
     }
 
-    // connect to database
-    $db = $this->connect();
-
     // create sql
     $sql = 'WITH search AS (SELECT' .
         ' ST_SetSRID(ST_MakePoint(:longitude,:latitude),4326)::geography' .
         ' AS point' .
         ')';
+
     // bound parameters
     $params = array(
         ':latitude' => $query->latitude,
@@ -53,21 +68,23 @@ class PlacesFactory extends GeoserveFactory {
         ' JOIN admin1_codes_ascii ON (geoname.country_code || \'.\' ||' .
             ' geoname.admin1_code = admin1_codes_ascii.code)'.
         ' JOIN country_info ON (geoname.country_code = country_info.iso)';
+
     // build where clause
     $where = array();
     if ($query->maxradiuskm !== null) {
       $where[] = 'ST_DWithin(search.point, geoname.shape, :distance)';
       $params[':distance'] = $query->maxradiuskm * 1000;
     }
-    if ($query->minpopulation !== null) {
-      $where[] = 'geoname.population >= :minpopulation';
-      $params[':minpopulation'] = $query->minpopulation;
-    }
+
+    $this->_buildGenericWhere($query, $where, $params);
+
     if (count($where) > 0) {
       $sql .= ' WHERE ' . implode(' AND ', $where);
     }
+
     // sort closest places first
     $sql .= ' ORDER BY distance';
+
     // limit number of results
     if ($query->limit !== null) {
       $sql .= ' LIMIT :limit';
@@ -75,7 +92,100 @@ class PlacesFactory extends GeoserveFactory {
     }
 
     // execute query
+    return $this->_execute($sql, $params, $callback);
+  }
+
+  /**
+   * Get places for rectangle searches.
+   *
+   * Note :: Unlike with cicles, there is no azimuth or distance generated
+   * since there is no "center" of interest per-se.
+   *
+   */
+  public function getByRectangle ($query, $callback = null) {
+    if ($query->minlatitude === null || $query->maxlatitude === null ||
+        $query->minlongitude === null || $query->maxlongitude === null) {
+      throw new Exception('"minlatitude", "maxlatitude", "minlongitude", ',
+          '"maxlongitude" are required parameters');
+    }
+
+    $sql = '
+    SELECT
+      geoname.*,
+      admin1_codes_ascii.name AS admin1_name,
+      country_info.country AS country_name
+    FROM
+      geoname
+    JOIN
+      admin1_codes_ascii ON (geoname.country_code || \'.\' || ' .
+          'geoname.admin1_code = admin1_codes_ascii.code)
+    JOIN
+      country_info ON (geoname.country_code = country_info.iso)
+    WHERE
+    ';
+
+    $where = array();
+    $params = array(
+      'minlatitude' => $query->minlatitude,
+      'maxlatitude' => $query->maxlatitude,
+      'minlongitude' => $this->_normalizeLongitude($query->minlongitude),
+      'maxlongitude' => $this->_normalizeLongitude($query->maxlongitude)
+    );
+
+    if ($params['minlongitude'] > $params['maxlongitude']) {
+      // crosses dateline, make two boxes
+      $sql .= '((
+          geoname.shape &&
+          ST_MakeEnvelope(:minlongitude, :minlatitude, 180, :maxlatitude, 4326)
+        ) OR (
+          geoname.shape &&
+          ST_MakeEnvelope(-180, :minlatitude, :maxlongitude, :maxlatitude, 4326)
+      ))';
+    } else {
+      $sql .= '(geoname.shape && ST_MakeEnvelope(' .
+          ':minlongitude, :minlatitude, :maxlongitude, :maxlatitude, 4326))';
+    }
+
+    $this->_buildGenericWhere($query, $where, $params);
+
+    if (count($where) > 0) {
+      $sql .= ' AND ' . implode(' AND ', $where);
+    }
+
+    // sort populous places first
+    $sql .= ' ORDER BY geoname.population DESC';
+
+    // limit number of results
+    if ($query->limit !== null) {
+      $sql .= ' LIMIT :limit';
+      $params[':limit'] = $query->limit;
+    }
+
+    // execute query
+    return $this->_execute($sql, $params, $callback);
+  }
+
+  /**
+   * Generic method for parsing "WHERE" parameters that are common to both
+   * circle and rectangle searches.
+   *
+   */
+  private function _buildGenericWhere ($query, &$where, &$params) {
+    if ($query->minpopulation !== null) {
+      $where[] = 'geoname.population >= :minpopulation';
+      $params[':minpopulation'] = $query->minpopulation;
+    }
+  }
+
+  /**
+   * Excutes the query either invoking the callback if provided or returning
+   * the result set otherwise.
+   *
+   */
+  private function _execute ($sql, $params, $callback = null) {
+    $db = $this->connect();
     $query = $db->prepare($sql);
+
     if (!$query->execute($params)) {
       // handle error
       $errorInfo = $db->errorInfo();
@@ -87,19 +197,39 @@ class PlacesFactory extends GeoserveFactory {
     } else {
       try {
         if ($callback !== null) {
-          // use callback
           $callback->onStart($query);
+
           while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
             $callback->onPlace($row, $this);
           }
+
           $callback->onEnd();
         } else {
-          // return all places
           return $query->fetchAll(PDO::FETCH_ASSOC);
         }
       } finally {
         $query->closeCursor();
       }
     }
+  }
+
+  /**
+   * Maps the input longitude into a [-180,.0 +180.0] range.
+   *
+   */
+  private function _normalizeLongitude ($longitude) {
+    if ($longitude === null) {
+      return null;
+    }
+
+    while ($longitude < -180.0) {
+      $longitude += 360.0;
+    }
+
+    while ($longitude > 180.0) {
+      $longitude -= 360.0;
+    }
+
+    return $longitude;
   }
 }
