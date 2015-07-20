5378b15de4f4f719ec833b8c81630cc7577620a8
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-07-20 08:50:17 -0600

    added support for neic

--- src/lib/classes/RegionsFactory.class.php
+++ src/lib/classes/RegionsFactory.class.php
@@ -5,7 +5,9 @@ class RegionsFactory extends GeoserveFactory {
   protected static $SUPPORTED_TYPES = array(
     'admin',
     'authoritative',
-    'fe'
+    'fe',
+    'neic_catalog',
+    'neic_response'
   );
 
   /**
@@ -27,6 +29,12 @@ class RegionsFactory extends GeoserveFactory {
     }
     if (in_array('fe', $query->type)) {
       $data['fe'] = $this->getFE($query);
+    }
+    if (in_array('neic_catalog', $query->type)) {
+      $data['neic_catalog'] = $this->getNEICCatalog($query);
+    }
+    if (in_array('neic_response', $query->type)) {
+      $data['neic_response'] = $this->getNEICResponse($query);
     }
 
     return $data;
@@ -158,6 +166,70 @@ class RegionsFactory extends GeoserveFactory {
         ' ORDER BY priority ASC, ST_Area(shape) ASC';
 
     return $this->execute($sql, $params);
+  }
+
+  /**
+   * Get NEIC Regions
+   *
+   * @param $query {RegionsQuery}
+   *        query object
+   * @param $NEIC {String}
+   *        'neic_catalog' or 'neic_response'
+   */
+  public function getNEIC ($query, $NEIC) {
+    //Checks for latitude and longitude
+    if ($query->latitude === null || $query->longitude === null) {
+      throw new Exception('"latitude", and "longitude" are required');
+    }
+
+    // create sql
+    $sql = 'WITH search AS (SELECT' .
+        ' ST_SetSRID(ST_MakePoint(:longitude,:latitude),4326)::geography' .
+        ' AS point' .
+        ')';
+    // bound parameters
+    $params = array(
+      ':latitude' => $query->latitude,
+      ':longitude' => $query->longitude
+    );
+
+    $sql .= ' SELECT' .
+        ' name as name' .
+        ', magnitude as magnitude' .
+        ', id';
+
+    if ($query->includeGeometry) {
+      $sql .= ', ST_AsGeoJSON(shape) as shape';
+    }
+
+    $sql .= ' FROM search, ' . $NEIC .
+        ' WHERE search.point && shape' .
+        ' AND ST_Intersects(search.point, shape)' .
+        ' ORDER BY ST_Area(shape) ASC';
+
+    return $this->execute($sql, $params);
+  }
+
+  /**
+   * Get NEIC catalog Regions
+   * calls getNEIC
+   *
+   * @param $query {RegionsQuery}
+   *        query object
+   */
+  public function getNEICCatalog ($query) {
+    return $this->getNEIC($query, 'neic_catalog');
+  }
+
+  /**
+   * Get NEIC Response Regions
+   * calls getNEIC
+   *
+   * @param $query {RegionsQuery}
+   *        query object
+   */
+  public function getNEICResponse ($query) {
+    return $this->getNEIC($query, 'neic_response');
   }
 
 }
