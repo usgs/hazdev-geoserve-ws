41178daa8c7fcfd37c45f550e595b739e42045e7
Hal Simpson <hasimpson@usgs.gov>
2015-09-15 10:17:23 -0600

    Added schema for tectonic summary

---
+++ src/lib/sql/pgsql/tectonic_summary.sql
@@ -0,0 +1,28 @@
+------------------------------------------------------------------------
+-- DROP - TECTONCIC SUMMARY
+------------------------------------------------------------------------
+
+/* Indexes */
+DROP INDEX IF EXISTS tectonic_summary_shape_index;
+
+/* Tables */
+DROP TABLE IF EXISTS tectonic_summary CASCADE;
+
+
+------------------------------------------------------------------------------
+-- CREATE - ADMIN REGION
+------------------------------------------------------------------------------
+
+/* Tables */
+CREATE TABLE tectonic_summary (
+  id       INTEGER PRIMARY KEY,
+  name     VARCHAR(255),
+  type     VARCHAR(255),
+  summary  text,
+  shape    GEOGRAPHY(GEOMETRY, 4326)
+
+);
+
+/* Indexes */
+CREATE INDEX tectonic_summary_shape_index ON
+  tectonic_summary USING GIST (shape);
