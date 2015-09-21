307e63a7d63d753dcdfe565e7c699d71d5d6dbcb
ehunter-usgs <ehunter@usgs.gov>
2015-09-21 15:00:17 -0600

    Create AuthoritativeRegionView with example page

---
+++ src/htdocs/js/geoserve/AuthoritativeRegionView.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var View = require('mvc/View'),
+		Model = require('mvc/Model'),
+
+		Util = require('util/Util');
+
+
+var AuthoritativeRegionView = function (params) {
+	var _this,
+			_initialize,
+
+			_data;
+
+	_this = View(params||{});
+
+	_initialize = function (params) {
+		params = params || {};
+
+		_data = params.data || Model({});
+
+		_this.el.className = 'authoritative-region';
+
+		_this.render();
+	};
+
+	_this.render = function () {
+		var region,
+				markup;
+
+		if (_data.get('name') === null && _data.get('network') === null) {
+			markup = '<p class="alert info">No Authoritative Region Data</p>';
+		} else {
+			region = _data.get();
+			markup = '<dl>' +
+						'<dt>Name (Network)</dt>' +
+						'<dd>' + region.name + ' (' +
+								region.network + ')</dd>' +
+					'</dl>';
+		}
+
+		_this.el.innerHTML = '<h3>Authoritative Region</h3>' + markup;
+	};
+
+  /**
+   * View destroy method.
+   */
+  _this.destroy = Util.compose(function () {
+    _data = null;
+    _initialize = null;
+    _this = null;
+  }, _this.destroy);
+
+
+	_initialize(params);
+	params = null;
+	return _this;
+
+};
+
+module.exports = AuthoritativeRegionView;
