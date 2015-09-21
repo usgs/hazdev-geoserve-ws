a4253ce0ac755f04fc5a13c54b5d2dff7290d573
Eric Martinez <emartinez@usgs.gov>
2015-09-21 10:19:57 -0600

    Added individual files for each end point.

---
+++ src/htdocs/places.php
@@ -0,0 +1,169 @@
+<?php
+if (!isset($TEMPLATE)) {
+  include_once '../lib/data/metadata.inc.php';
+  include_once 'functions.inc.php';
+
+  if ($format === 'json') {
+    // JSON output
+    header('Content-Type: application/json');
+
+    $CACHE_MAXAGE = 900;
+    include_once '../lib/cache.inc.php';
+
+    if (count($_GET) <= 1) {
+      // Usage
+      print str_replace("\\'", '"', json_encode($GEOSERVE_PLACES));
+    } else {
+      try {
+        include_once '../conf/service.inc.php';
+        $SERVICE->places($_GET);
+      } catch (Exception $e) {
+        trigger_error($e->getMessage());
+        $SERVICE->error('500', 'Server Error');
+      }
+    }
+    exit(0);
+  }
+
+  // HTML output
+  $TITLE = 'Geoserve Places Endpoint';
+  $HEAD = '<link rel="stylesheet" href="endpoint.css"/>';
+  $NAVIGATION = navGroup("<a href=\"${MOUNT_PATH}/\">Geoserve</a>",
+      implode('', $endpointLinks));
+
+  if (!function_exists('pinfo')) {
+    function enumeration ($values) {
+      $types = '';
+
+      foreach ($values as $value) {
+        $types .= '<dt><code>' . $value['name'] . '</code></dt>' .
+            '<dd>' . $value['description'] . '</dd>';
+      }
+
+      return '<dl class="vertical types">' . $types . '</dl>';
+    }
+
+    function pinfo ($p) {
+      $result = $p['description'];
+
+      if (isset($p['minimum']) && isset($p['maximum'])) {
+        $result .= ' [' . $p['minimum'] . ', ' . $p['maximum'] . '].';
+      } else if (isset($p['minimum'])) {
+        $result .= ' [' . $p['minimum'] . ', +Inf].';
+      } else if (isset($p['maximum'])) {
+        $result .= ' [-Inf, ' . $p['maximum'] . '].';
+      }
+
+      if (isset($p['values'])) {
+        $result .= enumeration($p['values']);
+      }
+
+      return $result;
+    }
+
+    function formatTitle ($title) {
+      $formatted = ucfirst($title);
+
+      if ($title === 'common') {
+        $formatted = 'Geonames/Event';
+      }
+
+      return $formatted;
+    }
+  }
+
+  include_once 'template.inc.php';
+}
+?>
+
+<?php print $GEOSERVE_PLACES['description']; ?>
+
+<h2 id="request">Request</h2>
+
+<h3>Syntax</h3>
+<p>
+  A geoserve <em>places</em> search takes the following form:
+</p>
+<pre>
+  <code><?php print $GEOSERVE_PLACES['url']; ?></code>
+</pre>
+
+<h3>Parameters</h3>
+<?php foreach ($GEOSERVE_PLACES['parameters'] as $title=>$info) : ?>
+  <h4 id="parameter-<?php print $title; ?>">
+    <?php print ucfirst($title); ?> Search Parameters
+  </h4>
+  <ul class="parameters vertical separator no-style">
+    <?php foreach ($info as $name=>$parameter) : ?>
+      <li id="<?php print $name; ?>">
+        <header>
+          <code><?php print $name; ?></code>
+          <small><?php print $parameter['type']; ?></small>
+        </header>
+        <section>
+          <?php
+            print pinfo($parameter);
+          ?>
+        </section>
+      </li>
+    <?php endforeach ?>
+  </ul>
+<?php endforeach ?>
+
+<h3>Notes</h3>
+<?php foreach ($GEOSERVE_PLACES['notes'] as $note) : ?>
+  <aside class="parameter-note">
+    <?php print $note; ?>
+  </aside>
+<?php endforeach ?>
+
+
+<h2 id="response">Response</h2>
+<p>
+  The response is formatted as one or more nested
+  <a href="http://geojson.org/geojson-spec.html#feature-collection-objects">
+  GeoJSON FeatureCollections</a>. Each nested GeoJSON FeatureCollections is
+  keyed by the request <code>type</code>.
+</p>
+
+<h3>Properties</h3>
+<p>
+  Each returned Feature in the GeoJSON FeatureCollection includes an id,
+  a geometry object with longitude, latitude, and elevation,
+  and a properties object with the following attributes:
+</p>
+
+<?php foreach ($GEOSERVE_PLACES['output'] as $title=>$output) : ?>
+  <h4><?php print formatTitle($title); ?></h4>
+  <ul class="parameters vertical separator no-style">
+    <?php foreach ($output as $name=>$info) : ?>
+      <li id="<?php print $title . '-' . $name; ?>">
+        <header>
+          <code><?php print $name; ?></code>
+          <small><?php print $info['type']; ?></small>
+        </header>
+        <section>
+          <?php print $info['description']; ?>
+        </section>
+      </li>
+    <?php endforeach ?>
+  </ul>
+<?php endforeach ?>
+
+<h2 id="example">Examples</h2>
+<p>
+  Below are example requests and responses that detail the nested GeoJSON
+  structure. Each type has a nested GeoJSON FeatureCollection that may contain
+  multiple GeoJSON features. Click the link for an example to see the response.
+</p>
+
+<ul>
+  <?php foreach ($GEOSERVE_PLACES['examples'] as $example) : ?>
+    <li>
+      <p><?php print $example['description']; ?>:</p>
+      <a href="<?php print $example['url']; ?>">
+        <?php print $example['url']; ?>
+      </a>
+    </li>
+  <?php endforeach ?>
+</ul>
