f7903464f6b52043f39269eafc954d568945e15c
Eddie McWhirter <emcwhirter@usgs.gov>
2015-09-22 14:11:13 -0600

    Add basic markup with a header.

--- example/TectonicSummaryView.js
+++ example/TectonicSummaryView.js
@@ -9,5 +9,15 @@ _initialize = function () {
 
 };
 
+TectonicSummaryView({
+  header: '<h2>Example with data</h2>',
+  el: document.querySelector('#example')
+});
+
+TectonicSummaryView({
+  header: '<h2>Example without data</h2>',
+  el: document.querySelector('#example-nodata')
+});
+
 
 _initialize();
--- src/htdocs/js/geoserve/TectonicSummaryView.js
+++ src/htdocs/js/geoserve/TectonicSummaryView.js
@@ -4,19 +4,43 @@ var View = require('mvc/View'),
 
     Util = require('util/Util');
 
-    
+
 var _DEFAULTS = {
+  header: '<h2>Tectonic Summary View</h2>'
 };
 
 var TectonicSummaryView = function (options) {
   var _this,
-      _initialize;
+      _initialize,
+
+      _header;
 
 
   _this = View(options || {});
 
+  /**
+   * @Constructor
+   *
+   */
   _initialize = function (options) {
     options = Util.extend({}, _DEFAULTS, options);
+
+    _header = options.header;
+
+    _this.render();
+  };
+
+
+  _this.render = function () {
+    var markup;
+
+    markup = [];
+    
+    markup.push(
+      'Summary data here'
+    );
+
+    _this.el.innerHTML = markup.join('');
   };
 
   _initialize(options);
