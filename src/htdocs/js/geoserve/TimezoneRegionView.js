c1ee5cb6e3f9a03b359355a6c5b364beb951812c
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-09-22 16:17:50 -0600

    added suport for header and set defaults for it, updated to be more inline with other views

--- src/htdocs/js/geoserve/TimezoneRegionView.js
+++ src/htdocs/js/geoserve/TimezoneRegionView.js
@@ -1,66 +1,73 @@
 'use strict';
 
 var View = require('mvc/View'),
-    Model = require('mvc/Model'),
 
     Util = require('util/Util');
+
+var DEFAULTS = {
+  header: null
+};
 
 var TimezoneRegionView = function (params) {
   var _this,
       _initialize,
 
-      _data;
+      _header;
 
   _this = View(params || {});
 
-  _initialize = function (params) {
-    params = params || {};
+  _initialize = function () {
+    params = Util.extend({}, DEFAULTS, params);
 
-    _data = params.regions || Model({});
+    _header = params.header;
 
+    _this.el.className = 'timezone-region';
     _this.render();
   };
 
   _this.render = function () {
-    var timeData;
+    var markup,
+        regions,
+        timeData;
 
-    timeData = _data.get('region').timezone.features;
+    timeData = null;
+    regions = _this.model.get('regions');
 
-    if (timeData === null) {
-      _this.el.innerHTML =[
-        '<h3>',
-          'Time Zone Info',
-        '</h3>',
-        '<p>Time zone data unavailable</p>'
-      ].join('');
-    } else {
-      _this.el.innerHTML = [
-        '<h3>',
-          'Time Zone Info',
-        '</h3>',
-        '<dl>',
-          '<dt>Time Zone</dt>',
-            '<dd>',timeData.name,'</dd>',
-          '<dt>Offset</td>',
-            '<dd>',timeData.offset,'</dd>',
-          '<dt>DST Start</dt>',
-            '<dd>',timeData.dststart,'</dd>',
-          '<dt>DST End</dt>',
-            '<dd>',timeData.dstend,'</dd>',
-          '<dt>DST Offset</dt>',
-            '<dd>',timeData.dstoffset,'</dd>',
-        '</dl>'
-      ].join('');
+    try {
+      timeData = regions.timezone.features[0].properties;
+    } catch (e) {
+      timeData = null;
     }
+
+    if (timeData === null || timeData === undefined) {
+      markup = '<p>Time zone data unavailable</p>';
+    } else {
+      markup = '<dl>' +
+            '<dt>Time Zone</dt>' +
+              '<dd>' + timeData.timezone + '</dd>' +
+            '<dt>Standard Offset</td>' +
+              '<dd>' + timeData.standardoffset + '</dd>' +
+            '<dt>DST Start</dt>' +
+              '<dd>' + timeData.dststart + '</dd>' +
+            '<dt>DST End</dt>' +
+              '<dd>' + timeData.dstend + '</dd>' +
+            '<dt>DST Offset</dt>' +
+              '<dd>' + timeData.dstoffset + '</dd>' +
+          '</dl>';
+    }
+
+    _this.el.innerHTML = _header + markup;
   };
 
+  /**
+   * Destroy all the things
+   */
   _this.destroy = Util.compose(function () {
-    _data = null;
     _initialize = null;
     _this = null;
   }, _this.destroy);
 
-  _initialize(params);
+  _initialize();
   params = null;
   return _this;
 };
