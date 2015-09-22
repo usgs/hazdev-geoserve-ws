eba1098cc69ff4ce6e31bc7124032faec7f7bd7b
Eric Martinez <emartinez@usgs.gov>
2015-09-22 10:56:43 -0600

    Initial implementation of NeicResponseView.

---
+++ src/htdocs/js/geoserve/NeicResponseView.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var View = require('mvc/View'),
+
+    Util = require('util/Util');
+
+
+var _NO_DATA_MESSAGE = '<p class="alert info">No data to display.</p>';
+
+
+// Default values to be used by constructor
+var _DEFAULTS = {
+   // ... some defaults ...
+};
+
+
+/**
+ * Class: NeicResponseView
+ *
+ * @param params {Object}
+ *      Configuration options. See _DEFAULTS for more details.
+ */
+var NeicResponseView = function (params) {
+  var _this,
+      _initialize,
+
+      _formatMagnitude;
+
+
+  // Inherit from parent class
+  _this = View(params||{});
+
+  /**
+   * @constructor
+   *
+   */
+  _initialize = function (params) {
+    params = Util.extend({}, _DEFAULTS, params);
+    _this.render();
+  };
+
+
+  _formatMagnitude = function (magnitude) {
+    // TODO :: Use generic formatter here ...
+    return magnitude.toFixed(1);
+  };
+
+
+  _this.render = function () {
+    var markup,
+        neiccatalog,
+        properties;
+
+    try {
+      neiccatalog = _this.model.get('regions').neiccatalog;
+      properties = neiccatalog.features[0].properties;
+
+      markup = [
+        '<dl>',
+          '<dt>Name</dt>',
+            '<dd>', properties.name, '</dd>',
+          '<dt>Type</dt>',
+            '<dd>', properties.type, '</dd>',
+          '<dt>Magnitude</dt>',
+            '<dd>', _formatMagnitude(properties.magnitude), '</dd>',
+        '</dl>'
+      ].join('');
+    } catch (e) {
+      markup = _NO_DATA_MESSAGE;
+    }
+
+    _this.el.innerHTML = '<h3>NEIC Response Region</h3>' + markup;
+  };
+
+
+  // Always call the constructor
+  _initialize(params);
+  params = null;
+  return _this;
+};
+
+
+NeicResponseView.NO_DATA_MESSAGE = _NO_DATA_MESSAGE;
+
+module.exports = NeicResponseView;
