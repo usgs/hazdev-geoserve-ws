80b364def59ac6c8fb0e69b2229e4195b2e2ac31
Eddie McWhirter <emcwhirter@usgs.gov>
2015-09-22 14:28:54 -0600

    Add Model and Xhr to example page.

--- example/TectonicSummaryView.js
+++ example/TectonicSummaryView.js
@@ -1,23 +1,47 @@
 'use string';
 
-var TectonicSummaryView = require('geoserve/TectonicSummaryView');
+var TectonicSummaryView = require('geoserve/TectonicSummaryView'),
+
+    Model = require('mvc/Model'),
+
+    Xhr = require('util/Xhr');
 
 
-var _initialize;
+var _initialize,
+    _model;
 
 _initialize = function () {
+  _model = Model({
+    regions: null
+  });
 
+  TectonicSummaryView({
+    header: '<h2>Example with data</h2>',
+    el: document.querySelector('#example')
+  });
+
+  TectonicSummaryView({
+    header: '<h2>Example without data</h2>',
+    el: document.querySelector('#example-nodata')
+  });
+
+  Xhr.ajax({
+    url: 'regions.json',
+    success: _onRegions,
+    error: _onRegionsError,
+  });
 };
 
-TectonicSummaryView({
-  header: '<h2>Example with data</h2>',
-  el: document.querySelector('#example')
-});
 
-TectonicSummaryView({
-  header: '<h2>Example without data</h2>',
-  el: document.querySelector('#example-nodata')
-});
+_onRegions = function (regions) {
+  _model.set({regions: regions});
+};
 
+_onRegionsError = function () {
+  var p = document.body.appendChild(document.createElement('p'));
+
+  p.className = 'alert error';
+  p.innerHTML = 'Failed to load regions.json data.';
+};
 
 _initialize();
