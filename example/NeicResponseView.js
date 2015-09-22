8358e466a717a16e193144901f777860a36cb63b
Eric Martinez <emartinez@usgs.gov>
2015-09-22 10:57:17 -0600

    Added example page for NeicResponseView.

---
+++ example/NeicResponseView.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var NeicResponseView = require('geoserve/NeicResponseView'),
+
+    Model = require('mvc/Model'),
+
+    Xhr = require('util/Xhr');
+
+var _initialize,
+    _model,
+    _onRegions,
+    _onRegionsError;
+
+_initialize = function () {
+  _model = Model({regions: null});
+
+  NeicResponseView({
+    el: document.querySelector('#example'),
+    model: _model
+  });
+
+  NeicResponseView({
+    el: document.querySelector('#example-nodata'),
+    model: Model({regions: null})
+  });
+
+  Xhr.ajax({
+    url: 'regions.json',
+    success: _onRegions,
+    error: _onRegionsError,
+  });
+};
+
+
+_onRegions = function (regions) {
+  _model.set({regions: regions});
+};
+
+_onRegionsError = function () {
+  var p = document.body.appendChild(document.createElement('p'));
+
+  p.className = 'alert error';
+  p.innerHTML = 'Failed to load regions.json data.';
+};
+
+_initialize();
