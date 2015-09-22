27c7ec1600ae33c6e5a2058c82683d35bebcd76f
ehunter-usgs <ehunter@usgs.gov>
2015-09-22 14:08:51 -0600

    Create AdminRegionViewTest. Update AdminRegionView to have configurable header and no data message

---
+++ test/spec/geoserve/AdminRegionViewTest.js
@@ -0,0 +1,60 @@
+/* global chai, describe, it */
+'use strict';
+
+var AdminRegionView = require('geoserve/AdminRegionView'),
+		Model = require('mvc/Model');
+
+var expect = chai.expect;
+
+describe('AdminRegionView test suite.', function () {
+	describe('Constructor', function () {
+		it('can be created and destroyed', function () {
+			var createDestroy = function () {
+				var view = AdminRegionView();
+				view.destroy();
+			};
+
+			expect(createDestroy).to.not.throw('Error');
+		});
+	});
+
+	describe('View', function () {
+
+		it('shows custom message when no data is available', function () {
+			var div,
+					text;
+
+			div = document.createElement('div');
+			text = 'hello';
+
+			AdminRegionView({
+					el: div,
+					model: Model(),
+					noDataMessage: text
+				});
+
+			expect(div.querySelector('.alert').innerHTML).to.be.equal(text);
+		});
+
+
+		it('shows custom header when header is passed', function () {
+			var div,
+					header,
+					text;
+
+			div = document.createElement('div');
+			text = 'Header';
+			header = '<h3 class="header">' + text + '</h3>';
+
+			AdminRegionView({
+					el: div,
+					model: Model(),
+					header: header
+				});
+
+			expect(div.querySelector('.header').innerHTML).to.be.equal(text);
+		});
+
+
+	});
+});
