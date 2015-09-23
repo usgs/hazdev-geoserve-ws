4a04e86b43a7a4105a382f22c4589e4a4fbf00ea
ehunter-usgs <ehunter@usgs.gov>
2015-09-23 13:42:01 -0600

    Translate tabs to spaces

--- example/AuthoritativeRegionView.js
+++ example/AuthoritativeRegionView.js
@@ -8,28 +8,28 @@ var AuthoritativeRegionView = require('geoserve/AuthoritativeRegionView'),
 var model;
 
 model = Model({
-	region: null
+  region: null
 });
 
 AuthoritativeRegionView({
-	el: document.querySelector('#example1'),
-	model: model,
-	header: '<h3>Authoritative Region (With Data)</h3>'
+  el: document.querySelector('#example1'),
+  model: model,
+  header: '<h3>Authoritative Region (With Data)</h3>'
 });
 
 AuthoritativeRegionView({
-	el: document.querySelector('#example2'),
-	model: Model({
-		regions: null
-	}),
-	header: '<h3>Authoritative Region (Without Data)</h3>'
+  el: document.querySelector('#example2'),
+  model: Model({
+    regions: null
+  }),
+  header: '<h3>Authoritative Region (Without Data)</h3>'
 });
 
 Xhr.ajax({
-	url: 'regions.json',
-	success: function (data) {
-		model.set({
-			region: data
-		});
-	}
+  url: 'regions.json',
+  success: function (data) {
+    model.set({
+      region: data
+    });
+  }
 });
--- test/spec/geoserve/AuthoritativeRegionViewTest.js
+++ test/spec/geoserve/AuthoritativeRegionViewTest.js
@@ -2,59 +2,59 @@
 'use strict';
 
 var AuthoritativeRegionView = require('geoserve/AuthoritativeRegionView'),
-		Model = require('mvc/Model');
+    Model = require('mvc/Model');
 
 var expect = chai.expect;
 
 describe('AuthoritativeRegionView test suite.', function () {
-	describe('Constructor', function () {
-		it('can be created and destroyed', function () {
-			var createDestroy = function () {
-				var view = AuthoritativeRegionView();
-				view.destroy();
-			};
+  describe('Constructor', function () {
+    it('can be created and destroyed', function () {
+      var createDestroy = function () {
+        var view = AuthoritativeRegionView();
+        view.destroy();
+      };
 
-			expect(createDestroy).to.not.throw('Error');
-		});
-	});
+      expect(createDestroy).to.not.throw('Error');
+    });
+  });
 
-	describe('View', function () {
+  describe('View', function () {
 
-		it('shows custom message when no data is available', function () {
-			var div,
-					text;
+    it('shows custom message when no data is available', function () {
+      var div,
+          text;
 
-			div = document.createElement('div');
-			text = 'hello';
+      div = document.createElement('div');
+      text = 'hello';
 
-			AuthoritativeRegionView({
-					el: div,
-					model: Model(),
-					noDataMessage: text
-				});
+      AuthoritativeRegionView({
+          el: div,
+          model: Model(),
+          noDataMessage: text
+        });
 
-			expect(div.querySelector('.alert').innerHTML).to.be.equal(text);
-		});
-
-
-		it('shows custom header when header is passed', function () {
-			var div,
-					header,
-					text;
-
-			div = document.createElement('div');
-			text = 'Header';
-			header = '<h3 class="header">' + text + '</h3>';
-
-			AuthoritativeRegionView({
-					el: div,
-					model: Model(),
-					header: header
-				});
-
-			expect(div.querySelector('.header').innerHTML).to.be.equal(text);
-		});
+      expect(div.querySelector('.alert').innerHTML).to.be.equal(text);
+    });
 
 
-	});
+    it('shows custom header when header is passed', function () {
+      var div,
+          header,
+          text;
+
+      div = document.createElement('div');
+      text = 'Header';
+      header = '<h3 class="header">' + text + '</h3>';
+
+      AuthoritativeRegionView({
+          el: div,
+          model: Model(),
+          header: header
+        });
+
+      expect(div.querySelector('.header').innerHTML).to.be.equal(text);
+    });
+
+
+  });
 });
