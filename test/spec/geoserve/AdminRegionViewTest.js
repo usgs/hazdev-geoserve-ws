8e65742ce96e78be3fcd65a0f98f66c2751d5b82
ehunter-usgs <ehunter@usgs.gov>
2015-09-23 12:58:39 -0600

    Translate tabs to spaces, update describe, change "Error" to Error

--- test/spec/geoserve/AdminRegionViewTest.js
+++ test/spec/geoserve/AdminRegionViewTest.js
@@ -2,59 +2,59 @@
 'use strict';
 
 var AdminRegionView = require('geoserve/AdminRegionView'),
-		Model = require('mvc/Model');
+    Model = require('mvc/Model');
 
 var expect = chai.expect;
 
 describe('AdminRegionView test suite.', function () {
-	describe('Constructor', function () {
-		it('can be created and destroyed', function () {
-			var createDestroy = function () {
-				var view = AdminRegionView();
-				view.destroy();
-			};
+  describe('Constructor', function () {
+    it('can be created and destroyed', function () {
+      var createDestroy = function () {
+        var view = AdminRegionView();
+        view.destroy();
+      };
 
-			expect(createDestroy).to.not.throw('Error');
-		});
-	});
+      expect(createDestroy).to.not.throw(Error);
+    });
+  });
 
-	describe('View', function () {
+  describe('Render', function () {
 
-		it('shows custom message when no data is available', function () {
-			var div,
-					text;
+    it('shows custom message when no data is available', function () {
+      var div,
+          text;
 
-			div = document.createElement('div');
-			text = 'hello';
+      div = document.createElement('div');
+      text = 'hello';
 
-			AdminRegionView({
-					el: div,
-					model: Model(),
-					noDataMessage: text
-				});
+      AdminRegionView({
+          el: div,
+          model: Model(),
+          noDataMessage: text
+        });
 
-			expect(div.querySelector('.alert').innerHTML).to.be.equal(text);
-		});
-
-
-		it('shows custom header when header is passed', function () {
-			var div,
-					header,
-					text;
-
-			div = document.createElement('div');
-			text = 'Header';
-			header = '<h3 class="header">' + text + '</h3>';
-
-			AdminRegionView({
-					el: div,
-					model: Model(),
-					header: header
-				});
-
-			expect(div.querySelector('.header').innerHTML).to.be.equal(text);
-		});
+      expect(div.querySelector('.alert').innerHTML).to.be.equal(text);
+    });
 
 
-	});
+    it('shows custom header when header is passed', function () {
+      var div,
+          header,
+          text;
+
+      div = document.createElement('div');
+      text = 'Header';
+      header = '<h3 class="header">' + text + '</h3>';
+
+      AdminRegionView({
+          el: div,
+          model: Model(),
+          header: header
+        });
+
+      expect(div.querySelector('.header').innerHTML).to.be.equal(text);
+    });
+
+
+  });
 });
