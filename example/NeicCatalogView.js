8ecf5edd0e8350229027e6a8be58e94a0262e819
Hal Simpson <hasimpson@usgs.gov>
2015-09-23 10:14:29 -0600

    Instatiated model with null,  then reset with ajax call

--- example/NeicCatalogView.js
+++ example/NeicCatalogView.js
@@ -1,20 +1,47 @@
 'use strict';
 
 var NeicCatalogView = require('geoserve/NeicCatalogView'),
+
     Model = require('mvc/Model'),
+
     Xhr = require('util/Xhr');
 
-Xhr.ajax({
-  url: 'regions.json',
-  success: function (regions) {
-    NeicCatalogView({
-      el: document.querySelector('#example'),
-      model: Model({regions:regions}),
-    });
-  },
-  error: function () {
-    var p = document.body.appendChild(document.createElement('p'));
-    p.className = 'alert error';
-    p.innerHTML = 'Failed to load regions.json data.';
-  }
-});
+var _initialize,
+    _model,
+    _onRegions,
+    _onRegionsError;
+
+_initialize = function () {
+  _model = Model({regions:null});
+
+  NeicCatalogView({
+    header: '<h2>Example with data</h2>',
+    el: document.querySelector('#example'),
+    model: _model
+  });
+
+  NeicCatalogView({
+    header: '<h2>Example without data</h2>',
+    el: document.querySelector('#example-nodata'),
+    model: Model({regions: null})
+  });
+
+  Xhr.ajax({
+    url: 'regions.json',
+    success: _onRegions,
+    error: _onRegionsError
+  });
+};
+
+_onRegions = function (regions) {
+  _model.set({regions: regions});
+};
+
+_onRegionsError = function () {
+  var p = document.body.appendChild(document.createElement('p'));
+
+  p.className = 'alert error';
+  p.innerHTML = 'Failed to load regions.json data.';
+};
+
+_initialize();
