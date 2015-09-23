f62c16e0db089102dac77c9919ff8cd685531045
ehunter-usgs <ehunter@usgs.gov>
2015-09-23 12:41:54 -0600

    translate tabs to spaces

--- example/AdminRegionView.js
+++ example/AdminRegionView.js
@@ -2,26 +2,26 @@
 
 var AdminRegionView = require('geoserve/AdminRegionView'),
 
-		Model = require('mvc/Model'),
-		Xhr = require('util/Xhr');
+    Model = require('mvc/Model'),
+    Xhr = require('util/Xhr');
 
 Xhr.ajax({
-	url: 'regions.json',
-	success: function (data) {
-		AdminRegionView({
-			el: document.querySelector('#example'),
-			model: Model({
-				regions: data
-			}),
-			header: '<h3>Administrative Region (With Data)</h3>'
-		});
+  url: 'regions.json',
+  success: function (data) {
+    AdminRegionView({
+      el: document.querySelector('#example'),
+      model: Model({
+        regions: data
+      }),
+      header: '<h3>Administrative Region (With Data)</h3>'
+    });
 
-		AdminRegionView({
-			el: document.querySelector('#example2'),
-			model: Model({
-				regions: null
-			}),
-			header: '<h3>Administrative Region (Without Data)</h3>'
-		});
-	}
+    AdminRegionView({
+      el: document.querySelector('#example2'),
+      model: Model({
+        regions: null
+      }),
+      header: '<h3>Administrative Region (Without Data)</h3>'
+    });
+  }
 });
