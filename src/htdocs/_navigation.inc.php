5e4479bc084375e0801921d5fe683953eb730cc3
Eric Martinez <emartinez@usgs.gov>
2015-09-30 10:05:35 -0600

    Switching left nav link to use php for services.

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -5,7 +5,7 @@ include_once '../lib/data/metadata.inc.php';
 
 echo navItem($MOUNT_PATH . '/index.php', 'Geoserve');
 echo navGroup(
-    navItem($MOUNT_PATH . '/services.html', 'Geoserve Documentation'),
+    navItem($MOUNT_PATH . '/services.php', 'Geoserve Documentation'),
     implode("", $endpointLinks));
 
 ?>
